741e9b90c4075e7b9d57f45cad110085a546e95c
mzp <mzpppp@gmail.com>
2022-03-13 15:26:11 -0700

    :octocat: setup github action for testing and linting

--- Scripts/swiftlint.sh
+++ Scripts/swiftlint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-cd BuildTools
-SDKROOT=(xcrun --sdk macosx --show-sdk-path)
-swift run -c release swiftlint "$SRCROOT"
-  
+if [[ $BISMUSH_SKIP_TEST == 1 ]]; then
+    echo "Skip swiftlint because BISMUSH_SKIP_TEST is set"
+else
+    cd BuildTools
+    SDKROOT=(xcrun --sdk macosx --show-sdk-path)
+    swift run -c release swiftlint "$SRCROOT"
+fi
