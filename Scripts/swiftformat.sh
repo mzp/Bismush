52ee058e643a40882215fe9e5413c8dacbe77a6c
MIZUNO Hiroki <mzpppp@gmail.com>
2022-03-13 15:35:29 -0700

    :octocat: setup github action for testing and linting (#3)

--- Scripts/swiftformat.sh
+++ Scripts/swiftformat.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-cd BuildTools
-SDKROOT=(xcrun --sdk macosx --show-sdk-path)
-swift run -c release swiftformat "$SRCROOT"
+if [[ $BISMUSH_SKIP_TEST == 1 ]]; then
+    echo "Skip swiftformat because BISMUSH_SKIP_TEST is set"
+else
+    cd BuildTools
+    SDKROOT=(xcrun --sdk macosx --show-sdk-path)
+    swift run -c release swiftformat "$SRCROOT"
+fi
