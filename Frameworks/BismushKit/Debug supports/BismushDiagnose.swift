55268fd514c68f08b27d27f7fe075c1dea48ca6c
mzp <mzpppp@gmail.com>
2022-06-25 11:40:50 -0700

    fix build error on github action

--- Frameworks/BismushKit/Debug supports/BismushDiagnose.swift
+++ Frameworks/BismushKit/Debug supports/BismushDiagnose.swift
@@ -46,8 +46,6 @@ enum BismushDiagnose {
             return "Apple 6"
         case .apple7:
             return "Apple 7"
-        case .apple8:
-            return "Apple 8"
         case .mac2:
             return "mac 2"
         case .macCatalyst1:
@@ -56,6 +54,8 @@ enum BismushDiagnose {
             return "mac Catalyst 2"
         // swiftlint:disable switch_case_alignment
         #if swift(>=5.7) // i.e. >= Xcode14
+            case .apple8:
+                return "Apple 8"
             case .metal3:
                 return "metal 3"
         #endif
@@ -81,15 +81,29 @@ enum BismushDiagnose {
             ].first(where: { device.supportsFamily($0) })
         #endif
 
-        let appleGPUFamily = [
-            MTLGPUFamily.apple7,
-            .apple6,
-            .apple5,
-            .apple4,
-            .apple3,
-            .apple2,
-            .apple1,
-        ].first(where: device.supportsFamily)
+        #if swift(>=5.7) // i.e. >= Xcode14
+            let appleGPUFamily = [
+                .apple8,
+                .apple7,
+                .apple6,
+                .apple5,
+                .apple4,
+                .apple3,
+                .apple2,
+                .apple1,
+            ].first(where: device.supportsFamily)
+        #else
+            let appleGPUFamily = [
+                MTLGPUFamily.apple7,
+                .apple6,
+                .apple5,
+                .apple4,
+                .apple3,
+                .apple2,
+                .apple1,
+            ].first(where: device.supportsFamily)
+
+        #endif
         let commonGPUFamily = [
             MTLGPUFamily.common3,
             .common2,
