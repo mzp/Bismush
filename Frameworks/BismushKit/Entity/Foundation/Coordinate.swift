4207d6c6954978ee0b8a30fa9e832e3f19f3cfc9
mzp <mzpppp@gmail.com>
2022-07-04 15:21:26 -0700

    Support Xcode13

--- Frameworks/BismushKit/Entity/Foundation/Coordinate.swift
+++ Frameworks/BismushKit/Entity/Foundation/Coordinate.swift
@@ -44,7 +44,7 @@ public struct Size<T: Coordinate>: Codable, CustomStringConvertible, Equatable, 
         rawValue = SIMD2(Float(cgSize.width), Float(cgSize.height))
     }
 
-    public init(_ other: Size<some Coordinate>) {
+    public init<U>(_ other: Size<U>) {
         rawValue = other.rawValue
     }
 
